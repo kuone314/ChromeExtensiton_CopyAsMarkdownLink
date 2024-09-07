326a709d942c4180c283bc03fbcdeaaa112d29ff
kuone <kuone@example.com>
2024-09-07 11:16:58 +0900

    とりあえず、コンテキストメニューの追加

--- src/background.ts
+++ src/background.ts
@@ -1,6 +1,31 @@
 function polling() {
-  // console.log("polling");
+  console.log("polling");
+
   setTimeout(polling, 1000 * 30);
 }
 
 polling();
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "sampleContextMenu",
+    title: "MarkDown",
+    contexts: ["selection"],
+    // "onclick": copytext()
+  });
+});
+
+// コンテキストメニューがクリックされたときの処理
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "sampleContextMenu") {
+    console.log("Context menu clicked!");
+    // ここにクリック時の動作を追加
+    alert("tst")
+
+  }
+});
+
+function copytext(): ((info: chrome.contextMenus.OnClickData, tab: chrome.tabs.Tab) => void) | undefined {
+  alert("tst")
+  return undefined;
+}
--- src/popup.tsx
+++ src/popup.tsx
@@ -32,12 +32,88 @@ const Popup = () => {
     });
   };
 
+  function testFnc(): void {
+    chrome.tabs.query(
+      { active: true, currentWindow: true },
+      function (tabs) { setCurrentURL(tabs[0].url); }
+    );
+
+    // https://stackoverflow.com/questions/19164474/chrome-extension-get-selected-text
+    // chrome.tabs.executeScript({ code: 'window.getSelection().toString();' }, selectedText => {
+    //   console.log(selectedText[0]);
+    // });
+
+    // chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    //   // chrome.tabs.sendMessage(tabs[0].id!, { action: "getSelectedText" }, (response) => {
+    //   //   // alert("a");
+    //   //   console.log("a")
+    //   //   console.log(response.toString());
+    //   //   // alert(response.toString());
+    //   // });
+
+    //   // chrome.tabs.sendMessage(tabs[0].id!, { action: "getSelectedText" }, (response) => {
+    //   //   // alert("a");
+    //   //   console.log("a")
+    //   //   console.log(response.toString());
+    //   //   // alert(response.toString());
+    //   // });
+    //   chrome.scripting.executeScript({
+    //     target: { tabId: tabs[0].id! },
+    //     function: getSelectedText,
+    //   }, (results) => {
+    //     if (results && results[0]) {
+    //       document.getElementById("selectedText")!.textContent = results[0].result;
+    //     }
+    //   });
+
+
+    //   function getSelectedText() {
+    //     return window?.getSelection()?.toString() ?? "error";
+    //   }
+    // });
+    console.log("ab")
+    console.log(window?.getSelection()?.toString() ?? "error")
+
+    // chrome.runtime.sendMessage({ action: "requestSelectedText" });
+
+    // chrome.tabs.getCurrent(
+    //   (tab)=>{
+    //     tab.get
+    //   }
+    // )
+
+    // chrome.tabs.getSelected(tab=>{  // 現在のタブを取得
+    //     Data.Title = tab.title;  // tabに現在のタブが格納されている（？）。
+    //     Data.URL = tab.url;    // tab.titleには現在開いているタブのページタイトルが、tab.urlにはURLが格納されている。
+    //     console.log(Title: ${Data.Title});  // 出力は、「ポップアップを検証」で見れる。
+    //     console.log(URL: ${Data.URL});
+    // });
+
+
+    // TODO:起動方法
+    // Pouupから
+    // TODO:選択文字列の取得
+    // const selectedStr = window.getSelection()?.toString() ?? "Error";
+    // alert(selectedStr);
+    // setCurrentURL(selectedStr);
+    // TODO:MarkDownのリンク形式に
+    // TODO:クリップボードにコピー
+
+    // currentURL = "" // NG!
+    // setCurrentURL(""); // OK
+  }
+
   return (
     <>
       <ul style={{ minWidth: "700px" }}>
         <li>Current URL: {currentURL}</li>
         <li>Current Time: {new Date().toLocaleTimeString()}</li>
       </ul>
+      <button
+        onClick={testFnc}
+      >
+        test
+      </button>
       <button
         onClick={() => setCount(count + 1)}
         style={{ marginRight: "5px" }}
